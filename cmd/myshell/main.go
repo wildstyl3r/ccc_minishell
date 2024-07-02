07cbe2246b232411c06ab52255902e44b13a4d60
Mike <wildstyl3r@yandex.ru>
2024-07-02 20:46:34 +0300

    unrecognized command message and repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"bufio"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"fmt"
 	"os"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+		// Wait for user input
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				return
+			}
+			fmt.Fprintln(os.Stdout, "error: ", err)
+		}
+		argv := strings.Fields(line)
+		if len(argv) > 0 {
+			fmt.Fprintln(os.Stdout, argv[0]+": command not found")
+		}
+
+	}
 }
